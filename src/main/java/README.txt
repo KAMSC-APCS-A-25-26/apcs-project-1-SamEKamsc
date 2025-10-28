e86f68a51567df08bf333578c88eeb378d6a6c81
SamEKamsc <03samuel.edmonds@kamsc.org>
2025-10-28 14:00:18 -0400

    Started

---
+++ src/main/java/README.txt
@@ -0,0 +1,2 @@
+Welcome to the race!
+created by Sam Edmonds
--- src/main/java/TextAdventure.java
+++ src/main/java/TextAdventure.java
@@ -1,7 +1,33 @@
 import java.util.Scanner;
+import java.io.File;
 
 public class TextAdventure {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("=======================================");
+        System.out.println("                  THE");
+        System.out.println("Γ ‾ ‾ ⅂    Γ ‾ ‾ ⅂    Γ ‾ ‾ ‾  Γ ‾ ‾ ‾");
+        System.out.println("|     |    |     |    |        |");
+        System.out.println("| — — ⅃    | — — |    |        | — — —");
+        System.out.println("| \\        |     |    |         |");
+        System.out.println("|   \\      |     |    L _ _ _   L _ _ _");
+        System.out.println("             BY SAM EDMONDS");
+        System.out.println("=======================================");
+        System.out.println("Welcome to the RACE!");
+        System.out.println("1. Play");
+        System.out.println("2. How to play");
+        var choice_one = sc.nextInt();
+        switch (choice_one) {
+            case 1:
+                System.out.println("run");
+            case 2:
+                Scanner file_sc = new Scanner(new File("src/main/java/README.txt"));
+                while (file_sc.hasNext()) {
+                    System.out.println(file_sc.next());
+                }
+
+        }
+
 
     }
 }
